6fcbeece95f6e4e389e329013c1821e4074995db
Alcy Torres <mxalcytorres@gmail.com>
2024-07-12 16:59:46 -0500

    Write all the belongs_to / has_many associations for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+
+  has_many :favorite_movies
+  has_many :movies, through: :favorite_movies
 end
